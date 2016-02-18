f29d50dbca84cc0594bf973f5d36a5fe8afaa095
Godfrey Got-Wai Chiu <ggwc82@gmail.com>
2016-02-18 16:54:46 +0000

    Fully implemented all features of thermostat, including temperature color

--- jasmine/src/thermostat.js
+++ jasmine/src/thermostat.js
@@ -7,18 +7,22 @@ function Thermostat() {
 
 Thermostat.prototype.increase = function(number) {
 	if (this.temp + number > this.maximum) { throw "Maximum temperature is " + this.maximum }
+  console.log("increase temp")
 		this.temp += number;
 }
 Thermostat.prototype.decrease = function(number) {
 	if (this.temp - number < 10) { throw "Below minimum temperature!"; };
+  console.log("decrease temp")
 	this.temp -= number;
 }
 
 Thermostat.prototype.powerSavingOn = function() {
+  console.log("pwerSaving on")
 	this.maximum = 25;
 }
 
 Thermostat.prototype.powerSavingOff = function() {
+  console.log("pwrSaving off")
 	this.maximum = 32;
 }
 
